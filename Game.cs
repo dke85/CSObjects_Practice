b5ab48f0c6bcd1e0b60bcd3d5db96ec20ab89edc
Dennis <11572625+dke85@users.noreply.github.com>
2021-05-16 21:12:46 -0400

    Updating changes from videos

--- Game.cs
+++ Game.cs
@@ -15,7 +15,18 @@ namespace ObjectPractice
             //MapLocation x = new MapLocation(4, 2);
             try
             {
-            MapLocation mapLocation = new MapLocation(20, 20, map);
+            //MapLocation mapLocation = new MapLocation(20, 20, map);
+                MapLocation[] path = 
+                {
+                    new MapLocation(0,2, map),
+                    new MapLocation(1,2, map),
+                    new MapLocation(2,2, map),
+                    new MapLocation(3,2, map),
+                    new MapLocation(4,2, map),
+                    new MapLocation(5,2, map),
+                    new MapLocation(6,2, map),
+                    new MapLocation(7,2, map)
+                };
             }
             catch(OutOfBoundsException ex)
             {
--- Path.cs
+++ Path.cs
@@ -2,6 +2,24 @@ namespace ObjectPractice{
     
     class Path
     {
-        
+        private readonly MapLocation[] _path;
+        public Path(MapLocation[] path)
+        {
+            _path = path;
+        }
+
+        public MapLocation GetLocationAt(int pathStep)
+        {
+            /*if(pathStep < _path.Length){
+                return _path[pathStep];
+            }
+            else
+            {
+                return null;
+            }*/
+
+            //parenthesis not needed below or could be added
+            return (pathStep < _path.Length) ? _path[pathStep] : null;
+        }
     }
 }
